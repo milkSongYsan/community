bf5fe140d482a1ce549b57919932a81c9adeba46
SongYsan <1610752156@qq.com>
2021-07-26 15:57:17 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -7,4 +7,20 @@
 [github文档](https://docs.github.com/cn/developers/)
 
 
-## test
+## 脚本
+
+```sql
+create table USER
+(
+	ID INT auto_increment,
+	ACCOUNT_ID VARCHAR(100),
+	NAME VARCHAR(50),
+	TOKEN CHAR(36),
+	GMT_CREATE BIGINT,
+	GMT_MODIFIED BIGINT,
+	constraint USER_PK
+		primary key (ID)
+);
+
+
+```
--- src/main/java/com/community/provider/GithubProvider.java
+++ src/main/java/com/community/provider/GithubProvider.java
@@ -24,8 +24,8 @@ public class GithubProvider {
 
             String token = string.split("&")[0].split("=")[1];
 
-            System.out.println(string);
-            return string;
+//            System.out.println(string);
+            return token;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -35,7 +35,8 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token=" + accessToken)
+                .url("https://api.github.com/user")
+                .header("Authorization", "token " + accessToken)
                 .build();
 
         try {
